28ae6d67a2402cc3e8a650fa65a6996d70b21c9f
Jun Mukai <mukai@google.com>
2016-06-06 16:36:48 -0700

    Modify the publishing script

--- publish-gh-pages.sh
+++ publish-gh-pages.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 original_branch=`git branch | grep '^\*' | sed -e 's/^\* //'`
+dry_run_command=''
+
+if [ "$1" = "-n" -o "$1" = "--dry_run" ]; then
+  dry_run_command='echo'
+fi
 
 remote_name=`git remote -v | grep 'git@github.com:googleapis/gax-nodejs.git' | sed -e 's/[[:space:]].*//' | head -n 1`
 if [ -z "${remote_name}" ]; then
@@ -13,27 +18,39 @@ fi
 # re-generate the doc files.
 npm run doc
 
-git checkout -b gh-pages "${remote_name}/gh-pages"
-
-if [ $? != 0 ]; then
-  echo "can't switch to gh-pages branch. Might be some files exist."
-  exit 0
+if [ $? -ne 0 ]; then
+  ehco "failed to generate the documents. Quiting..."
+  exit 1
 fi
 
-cp -rf doc/google-gax/*/* .
-
-diffs=`git diff`
-
-if [ -z "${diffs}" ]; then
-  echo 'nothing to be updated!'
-  exit 0
+if [ -n "${dry_run_command}" ]; then
+  echo "dry-run mode: the following commands will be executed."
+  echo ""
 fi
 
-echo "git diffs: "
-echo "$diffs"
+$dry_run_command git checkout -b gh-pages "${remote_name}/gh-pages"
 
-git commit -a -m "update gh-pages"
-git push ${remote_name} gh-pages
+if [ $? -ne 0 ]; then
+  echo "failed to switch to the branch. Quitting..."
+  exit 1
+fi
+
+$dry_run_command cp -rf doc/google-gax/*/* .
+
+if [ -z "${dry_run_command}" ]; then
+  diffs=`git diff`
+
+  if [ -z "${diffs}" ]; then
+    echo 'nothing to be updated!'
+    exit 0
+  fi
+
+  echo "git diffs: "
+  echo "$diffs"
+fi
+
+$dry_run_command git commit -a -m "update gh-pages"
+$dry_run_command git push ${remote_name} gh-pages
 
 echo "done"
-git checkout "${original_branch}"
+$dry_run_command git checkout "${original_branch}"
