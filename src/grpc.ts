3539b2c4dabd5093f3fc1dbeb43c402ac51c02ac
Alexander Fenster <github@fenster.name>
2018-09-12 11:15:22 -0700

    fix: reverting to use grpc.loadObject for now (#301)
    * debug output
    
    * debug output
    
    * add logging to operations_client.ts
    
    * use loadObject
    
    * no more logging

--- src/grpc.ts
+++ src/grpc.ts
@@ -198,20 +198,21 @@ export class GrpcClient {
    * @return {Object<string, *>} The gRPC loaded result (the toplevel namespace
    *   object).
    */
+
   loadProto(protoPath: string, filename: string) {
-    // This set of @grpc/proto-loader options
-    // 'closely approximates the existing behavior of grpc.load'
-    const includeDirs = INCLUDE_DIRS.slice();
-    includeDirs.unshift(protoPath);
-    const options = {
-      keepCase: true,
-      longs: String,
-      enums: String,
-      defaults: true,
-      oneofs: true,
-      includeDirs
-    };
-    return this.loadFromProto(filename, options);
+    const resolvedPath = GrpcClient._resolveFile(protoPath, filename);
+    const retval = this.grpc.loadObject(
+        protobuf.loadSync(resolvedPath, new GoogleProtoFilesRoot()));
+    return retval;
+  }
+
+  static _resolveFile(protoPath: string, filename: string) {
+    if (fs.existsSync(path.join(protoPath, filename))) {
+      return path.join(protoPath, filename);
+    } else if (COMMON_PROTO_FILES.indexOf(filename) > -1) {
+      return path.join(googleProtoFilesDir, filename);
+    }
+    throw new Error(filename + ' could not be found in ' + protoPath);
   }
 
   metadataBuilder(headers: OutgoingHttpHeaders) {
