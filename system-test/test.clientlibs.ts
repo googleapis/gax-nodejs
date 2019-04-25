d139b07ce03894d879d8824291ef4c692b678f76
Alexander Fenster <fenster@google.com>
2019-04-25 13:06:23 -0700

    test: end-to-end system test

--- system-test/test.clientlibs.ts
+++ system-test/test.clientlibs.ts
@@ -29,81 +29,19 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-import * as cp from 'child_process';
 import * as fs from 'fs';
+import {ncp} from 'ncp';
 import * as path from 'path';
 import * as rimraf from 'rimraf';
 import * as util from 'util';
+
+import {latestRelease, spawn} from './util';
 
 const mkdir = util.promisify(fs.mkdir);
 const rmrf = util.promisify(rimraf);
 
 const baseRepoUrl = 'https://github.com/googleapis/';
 const testDir = path.join(process.cwd(), '.system-test-run');
-
-interface ExecuteResult {
-  stdout: string;
-  stderr: string;
-}
-
-async function execute(command: string, cwd?: string): Promise<ExecuteResult> {
-  cwd = cwd || process.cwd();
-  const maxBuffer = 10 * 1024 * 1024;
-  console.log(`Execute: ${command} [cwd: ${cwd}]`);
-  return new Promise<ExecuteResult>((resolve, reject) => {
-    cp.exec(command, {cwd, maxBuffer}, (err, stdout, stderr) => {
-      if (err) {
-        reject(new Error(`Command ${command} terminated with error ${err}`));
-      } else {
-        resolve({stdout, stderr});
-      }
-    });
-  });
-}
-
-async function spawn(
-    command: string, args?: string[], cwd?: string): Promise<void> {
-  cwd = cwd || process.cwd();
-  args = args || [];
-  console.log(`Execute: ${command} ${args.join(' ')} [cwd: ${cwd}]`);
-  return new Promise<void>((resolve, reject) => {
-    const child = cp.spawn(command, args || [], {
-                      cwd,
-                      stdio: 'inherit'
-                    }).on('close', (code: number|null, signal: string|null) => {
-      if (code === 0) {
-        resolve();
-      } else {
-        reject(new Error(`Command ${command} terminated with code ${
-            code}, signal ${signal}`));
-      }
-    });
-  });
-}
-
-async function latestRelease(cwd: string): Promise<string> {
-  const gitTagOutput = (await execute('git tag --list', cwd)).stdout;
-  const tags =
-      gitTagOutput.split('\n')
-          .filter(str => str.match(/^v\d+\.\d+\.\d+$/))
-          .sort((tag1: string, tag2: string): number => {
-            const match1 = tag1.match(/^v(\d+)\.(\d+)\.(\d+)$/);
-            const match2 = tag2.match(/^v(\d+)\.(\d+)\.(\d+)$/);
-            if (!match1 || !match2) {
-              throw new Error(`Cannot compare git tags ${tag1} and ${tag2}`);
-            }
-            // compare major version, then minor versions, then patch versions.
-            // return positive number, zero, or negative number
-            for (let idx = 1; idx <= 3; ++idx) {
-              if (match1[idx] !== match2[idx]) {
-                return Number(match1[idx]) - Number(match2[idx]);
-              }
-            }
-            return 0;
-          });
-  // the last tag in the list is the latest release
-  return tags[tags.length - 1];
-}
 
 async function preparePackage(packageName: string): Promise<void> {
   await spawn(
