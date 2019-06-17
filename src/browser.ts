54c3f7e84427d435abc315920a1e5db54db8cb9c
Ramya Ramalingam <rramalingam@google.com>
2019-06-17 15:47:52 -0700

    changes

--- src/browser.ts
+++ src/browser.ts
@@ -1,3 +1,13 @@
+import { Gaxios } from "gaxios";
+
+export {
+    CallSettings,
+    constructSettings,
+    RetryOptions
+  } from './gax';
+
+export {createApiCall} from './createApiCall';
+  
 export function foo() {return 42;}
 
 export class GrpcClient {
@@ -5,3 +15,4 @@ export class GrpcClient {
     constructSettings() {}
     createStub() {}
 };
+
