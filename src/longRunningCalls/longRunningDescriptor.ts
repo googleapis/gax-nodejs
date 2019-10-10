ed466927b4825b2a85c7e86656dcad34fbf4ef95
Alexander Fenster <fenster@google.com>
2019-10-10 11:18:24 -0700

    fix: fix a type of AnyDecoder (#619)
    * fix: fix a type of AnyDecoder
    
    * fix LRO types

--- src/longRunningCalls/longRunningDescriptor.ts
+++ src/longRunningCalls/longRunningDescriptor.ts
@@ -29,18 +29,18 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import * as protobuf from 'protobufjs';
 import {Descriptor} from '../descriptor';
 import {CallSettings} from '../gax';
 import {Metadata} from '../grpc';
 import {OperationsClient} from '../operationsClient';
-
 import {LongrunningApiCaller} from './longRunningApiCaller';
 
 /**
  * A callback to upack a google.protobuf.Any message.
  */
 export interface AnyDecoder {
-  (message: {}): Metadata;
+  (reader: protobuf.Reader | Uint8Array, length?: number): protobuf.Message<{}>;
 }
 
 /**
