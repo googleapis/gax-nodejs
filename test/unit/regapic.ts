35d19c03eb246817ba9e784b2253aaf8f41a4f7f
summerji <summerji@google.com>
2021-06-23 22:34:23 -0700

    add mock response

--- test/unit/regapic.ts
+++ test/unit/regapic.ts
@@ -148,26 +148,21 @@ describe('regapic', () => {
         type: 'TYPEONE',
       };
       const requestObject = {shelf: shelf};
+      // incomplete types for nodeFetch, so...
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      sinon.stub(nodeFetch, 'Promise' as any).returns(
+        Promise.resolve({
+          ok: true,
+          arrayBuffer: () => {
+            return Promise.resolve(Buffer.from(JSON.stringify(shelf)));
+          },
+        })
+      );
       gaxGrpc.createStub(libraryService, stubOptions).then(libStub => {
         libStub.createShelf(requestObject, {}, {}, (err: {}) => {
           assert.strictEqual(err, null);
           done();
         });
-      });
-    });
-    it('should throw error with invalid enum in proto message request', done => {
-      const shelf = {
-        name: 'shelf-name',
-        theme: 'shelf-theme',
-        type: 'INVALID-TYPE',
-      };
-      const requestObject = {shelf: shelf};
-      gaxGrpc.createStub(libraryService, stubOptions).then(libStub => {
-        assert.throws(
-          () => libStub.createShelf(requestObject, {}, {}, {}),
-          Error
-        );
-        done();
       });
     });
   });
