ad29bc26567b588d5d694350844bea5de40963d6
Summer Ji <summerji@google.com>
2021-11-10 11:22:16 -0800

    fix: handle stream has no metadata event (#1132)

--- src/streamingCalls/streaming.ts
+++ src/streamingCalls/streaming.ts
@@ -17,6 +17,7 @@
 /* This file describes the gRPC-streaming. */
 
 import {Duplex, DuplexOptions, Readable, Stream, Writable} from 'stream';
+import {Metadata} from '@grpc/grpc-js';
 
 import {
   APICallback,
@@ -76,11 +77,19 @@ export enum StreamType {
   BIDI_STREAMING = 3,
 }
 
+interface Status {
+  code: number;
+  details: string;
+  message?: string;
+  metadata?: Metadata;
+}
+
 export class StreamProxy extends duplexify implements GRPCCallResult {
   type: StreamType;
   private _callback: APICallback;
   private _isCancelCalled: boolean;
   stream?: CancellableStream;
+  private _responseHasSent: boolean;
   /**
    * StreamProxy is a proxy to gRPC-streaming method.
    *
@@ -98,6 +107,7 @@ export class StreamProxy extends duplexify implements GRPCCallResult {
     this.type = type;
     this._callback = callback;
     this._isCancelCalled = false;
+    this._responseHasSent = false;
   }
 
   cancel() {
@@ -119,6 +129,19 @@ export class StreamProxy extends duplexify implements GRPCCallResult {
       stream.on(event, this.emit.bind(this, event));
     });
 
+    // gRPC is guaranteed emit the 'status' event but not 'metadata', and 'status' is the last event to emit.
+    // Emit the 'response' event if stream has no 'metadata' event.
+    // This avoids the stream swallowing the other events, such as 'end'.
+    stream.on('status', () => {
+      if (!this._responseHasSent) {
+        stream.emit('response', {
+          code: 200,
+          details: '',
+          message: 'OK',
+        });
+      }
+    });
+
     // We also want to supply the status data as 'response' event to support
     // the behavior of google-cloud-node expects.
     // see:
@@ -134,6 +157,7 @@ export class StreamProxy extends duplexify implements GRPCCallResult {
         message: 'OK',
         metadata,
       });
+      this._responseHasSent = true;
     });
   }
 
