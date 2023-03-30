0fb9d685205e64321d013c2ae5248a11df24ffe4
Sofia Leon <sofialeon@google.com>
2023-03-30 14:49:01 -0700

    delete cache before second require

--- test/unit/minify.ts
+++ test/unit/minify.ts
@@ -41,6 +41,7 @@ describe('minify tool', () => {
     const objectBefore = require(echoProtoJson);
     await minify.main(testDir);
     const statAfter = await fsp.stat(echoProtoJson);
+    delete require.cache[require(echoProtoJson)];
     const objectAfter = require(echoProtoJson);
     const contentAfter = (await fsp.readFile(echoProtoJson)).toString();
     const parsedObjectAfter = JSON.parse(contentAfter);
@@ -57,6 +58,7 @@ describe('minify tool', () => {
     const resultBefore = require(echoProtoJs);
     await minify.main(testDir);
     const statAfter = await fsp.stat(echoProtoJs);
+    delete require.cache[require(echoProtoJs)];
     const resultAfter = require(echoProtoJs);
     assert(statBefore.size > statAfter.size);
     assert.deepEqual(resultBefore, resultAfter);
