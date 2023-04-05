e098fab97bdced86a1175e23c34b499f5342ffc1
Sofia Leon <sofialeon@google.com>
2023-04-04 17:44:03 -0700

    feat: get tools scripts and dependencies running; still need to get top-level module running without tools, remove extra dependencies, and make sure to run commands from the tools dependency

--- tools/test/compileProtos.ts
+++ tools/test/compileProtos.ts
@@ -21,7 +21,7 @@ import * as rimraf from 'rimraf';
 import * as util from 'util';
 import * as path from 'path';
 import * as protobuf from 'protobufjs';
-import * as compileProtos from '../../tools/compileProtos';
+import * as compileProtos from '../compileProtos';
 
 const readFile = util.promisify(fs.readFile);
 const mkdir = util.promisify(fs.mkdir);
@@ -52,6 +52,7 @@ describe('compileProtos tool', () => {
 
   it('compiles protos to JSON, JS, TS', async function () {
     this.timeout(20000);
+    console.log(path.join(__dirname, '..', '..', 'test', 'fixtures', 'protoLists'))
     await compileProtos.main([
       path.join(__dirname, '..', '..', 'test', 'fixtures', 'protoLists'),
     ]);
