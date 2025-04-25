89bf4509310fc886eddf0360e3cec5d6b2fb7488
Daniel Bruce <djbruce@google.com>
2025-04-25 15:23:47 -0400

    Add a line of code that will help us see where the
    data is stringified

--- gax/test/test-application/src/index.ts
+++ gax/test/test-application/src/index.ts
@@ -509,6 +509,7 @@ async function testErrorDetailsWithTimeout(client: SequenceServiceClient) {
     await client.attemptSequence(attemptRequest, settings);
     assert.fail('The call should have failed');
   } catch (err) {
+    console.log('Stringifying response');
     console.log(JSON.stringify(err));
     assert.strictEqual((err as GoogleError).statusDetails, 'x');
   }
